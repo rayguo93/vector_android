009d6611a56ed8332c8f4fe0f920b14a1baff215
rayguo93 <ray.guo93@hotmail.com>
2015-02-19 15:48:44 -0500

    test2

---
+++ app/src/androidTest/java/com/practice/software/assignmentb/AndroidTest.java
@@ -0,0 +1,50 @@
+package com.practice.software.assignmentb;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.suitebuilder.annotation.MediumTest;
+
+import junit.framework.Assert;
+
+public class AndroidTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    private MainActivity mActivity;
+
+    public AndroidTest() {
+        super(MainActivity.class);
+    }
+
+    @Override
+    public void setUp() throws Exception{
+        super.setUp();
+        mActivity = getActivity();
+    }
+
+    @MediumTest
+    public void testCreateVector() {
+        Assert.assertTrue(mActivity.createVector(0, 0) instanceof Vector);
+    }
+
+    @MediumTest
+    public void testCreateVectorFields(){
+        double x1 = 0;
+        double y1 = 0;
+        double x2 = 10;
+        double y2 = 50;
+        double x3 = 50;
+        double y3 = -10;
+
+        Vector v1 = mActivity.createVector(x1,y1);
+        Vector v2 = mActivity.createVector(x2,y2);
+        Vector v3 = mActivity.createVector(x3,y3);
+
+        Assert.assertEquals(x1, v1.getX());
+        Assert.assertEquals(y1, v1.getY());
+        Assert.assertEquals(x2, v2.getX());
+        Assert.assertEquals(y2, v2.getY());
+        Assert.assertEquals(x3, v3.getX());
+        Assert.assertEquals(y3, v3.getY());
+    }
+
+
+
+}
---
+++ app/src/main/java/com/practice/software/assignmentb/Vector.java
@@ -0,0 +1,20 @@
+package com.practice.software.assignmentb;
+
+public class Vector {
+    private double x;
+    private double y;
+
+    public Vector(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+}
